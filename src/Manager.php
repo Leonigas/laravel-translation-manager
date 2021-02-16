8829bdb1299c6a16c0ac1be9e47fd7de586c0d92
Léopold Guérin <leopold.guerin@gmail.com>
2021-02-16 15:05:04 +0100

    Include .js files

--- src/Manager.php
+++ src/Manager.php
@@ -307,7 +307,7 @@ class Manager
 
         // Find all PHP + Twig files in the app folder, except for storage
         $finder = new Finder();
-        $finder->in($path)->exclude('storage')->exclude('vendor')->name('*.php')->name('*.twig')->name('*.vue')->files();
+        $finder->in($path)->exclude('storage')->exclude('node_modules')->exclude('vendor')->exclude('resources/assets')->exclude('public')->name('*.php')->name('*.twig')->name('*.vue')->name('*.js')->files();
 
         /** @var \Symfony\Component\Finder\SplFileInfo $file */
         foreach ($finder as $file) {
